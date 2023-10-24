dfbca129f762a241c27078d407f07b8fc9d5eec4
Kesehet <hamood.siddiqui@gmail.com>
2023-10-24 09:38:09 +0530

    beautified ...

--- Plaid-Test-Server/index.js
+++ Plaid-Test-Server/index.js
@@ -1,123 +1,108 @@
+// Imports
 const express = require('express');
-const app = express();
-const axios = require('axios');  // Add this line to import axios
-
+const axios = require('axios');
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-const PORT = process.env.PORT || 3000;
-
 const path = require('path');
 const util = require('util');
 const PLAID = require('plaid');
 require('dotenv').config();
 
-
+// Constants
+const app = express();
+const PORT = process.env.PORT || 3000;
+const CLIENT_ID = "65301fcba34480001b50a4a8";
+const SECRET = "fbfcfcf691aace4753f9e9db2aa207";
+const API_ENDPOINT = "https://sandbox.plaid.com";
 const plaidClient = new PLAID.PlaidApi({
-    clientID: "65301fcba34480001b50a4a8",
-    secret: "fbfcfcf691aace4753f9e9db2aa207",
+    clientID: CLIENT_ID,
+    secret: SECRET,
     env: "sandbox",
 });
 
+// Middleware
+app.use(bodyParser.json());
+
+// Helper Functions
+const sendPostRequest = async (url, data) => {
+    try {
+        const response = await axios.post(url, data, {
+            headers: { 'Content-Type': 'application/json' },
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error.response ? error.response.data : error);
+        throw error;
+    }
+};
+
+const logResponse = (title, data) => {
+    console.log('--------------------');
+    console.log(title);
+    console.log(util.inspect(data, false, null, true));
+};
+
+// Route Handlers
 app.get('/create-link-token', async (req, res) => {
     const requestPayload = {
-        client_id: "65301fcba34480001b50a4a8",
-        secret: "fbfcfcf691aace4753f9e9db2aa207",
-        user: {
-            client_user_id: "My User ID",
-        },
+        client_id: CLIENT_ID,
+        secret: SECRET,
+        user: { client_user_id: "My User ID" },
         client_name: "My Client Name",
-        products: ["auth","identity"],
+        products: ["auth", "identity"],
         country_codes: ["US"],
-        language: "en"
+        language: "en",
     };
-    
+
     try {
-        const response = await axios.post('https://sandbox.plaid.com/link/token/create', requestPayload, {
-            headers: {
-                'Content-Type': 'application/json',
-                // Add other necessary headers
-            }
-        });
-        const { link_token } = response.data;
+        const { link_token } = await sendPostRequest(`${API_ENDPOINT}/link/token/create`, requestPayload);
         res.json({ link_token });
     } catch (error) {
-        console.error(error);
         res.status(500).send('Server Error');
     }
 });
-
 
 app.post('/token-exchange', async (req, res) => {
     const { public_token } = req.body;
+    const commonPayload = {
+        client_id: CLIENT_ID,
+        secret: SECRET,
+    };
+
     try {
-        const response = await axios.post('https://sandbox.plaid.com/item/public_token/exchange', {
-            public_token: public_token,
-            client_id: "65301fcba34480001b50a4a8",
-            secret: "fbfcfcf691aace4753f9e9db2aa207",
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-            }
+        const { access_token, item_id } = await sendPostRequest(`${API_ENDPOINT}/item/public_token/exchange`, {
+            ...commonPayload,
+            public_token,
         });
 
-        const accessToken = response.data.access_token;
-        const itemID = response.data.item_id;
-
-        // Axios call for getAuth
-        const authResponse = await axios.post('https://sandbox.plaid.com/auth/get', {
-            client_id: "65301fcba34480001b50a4a8",
-            secret: "fbfcfcf691aace4753f9e9db2aa207",
-            access_token: accessToken
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-            }
+        const authResponse = await sendPostRequest(`${API_ENDPOINT}/auth/get`, {
+            ...commonPayload,
+            access_token,
         });
-        console.log('--------------------');
-        console.log('Auth Response');
-        console.log(util.inspect(authResponse.data, false, null, true));
+        logResponse('Auth Response', authResponse);
 
-        // Axios call for getIdentity
-        const identityResponse = await axios.post('https://sandbox.plaid.com/identity/get', {
-            client_id: "65301fcba34480001b50a4a8",
-            secret: "fbfcfcf691aace4753f9e9db2aa207",
-            access_token: accessToken
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-            }
+        const identityResponse = await sendPostRequest(`${API_ENDPOINT}/identity/get`, {
+            ...commonPayload,
+            access_token,
         });
-        console.log('--------------------');
-        console.log('Identity Response');
-        console.log(util.inspect(identityResponse.data, false, null, true));
+        logResponse('Identity Response', identityResponse);
 
-        // Axios call for getBalance
-        const balanceResponse = await axios.post('https://sandbox.plaid.com/accounts/balance/get', {
-            client_id: "65301fcba34480001b50a4a8",
-            secret: "fbfcfcf691aace4753f9e9db2aa207",
-            access_token: accessToken
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-            }
+        const balanceResponse = await sendPostRequest(`${API_ENDPOINT}/accounts/balance/get`, {
+            ...commonPayload,
+            access_token,
         });
-        console.log('--------------------');
-        console.log('Balance Response');
-        console.log(util.inspect(balanceResponse.data, false, null, true));
+        logResponse('Balance Response', balanceResponse);
 
         res.sendStatus(200);
     } catch (error) {
-        console.error(error.response ? error.response.data : error);
         res.status(500).send('Server Error');
     }
 });
-
 
 app.get('/', async (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Server Startup
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
